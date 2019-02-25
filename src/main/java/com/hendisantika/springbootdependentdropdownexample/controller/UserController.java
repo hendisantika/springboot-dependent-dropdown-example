5a379dc6a0c33b29da27e32f5895f861542617b0
hendisantika <hendisantika@yahoo.co.id>
2019-02-25 16:54:49 +0700

    #9 - Add User Controller

---
+++ src/main/java/com/hendisantika/springbootdependentdropdownexample/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.hendisantika.springbootdependentdropdownexample.controller;
+
+import com.hendisantika.springbootdependentdropdownexample.config.DropdownItemProperties;
+import com.hendisantika.springbootdependentdropdownexample.config.RadioButtonProperties;
+import com.hendisantika.springbootdependentdropdownexample.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-dependent-dropdown-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-25
+ * Time: 16:54
+ */
+@Controller
+public class UserController {
+    @Autowired
+    private DropdownItemProperties dropdownItemProperties;
+
+    @Autowired
+    private RadioButtonProperties radioButtonItemProperties;
+
+    @RequestMapping("/")
+    public String displayExample(User user, Model model) {
+
+        return "example";
+    }
+
+    @RequestMapping(value = "/save", params = {"save"}, method = RequestMethod.POST)
+    public String saveUser(@Valid User user, BindingResult bindingResult,
+                           Model model) {
+        System.out.println(user.toString());
+        return "redirect:/";
+    }
+
+    @ModelAttribute("userRoleOptions")
+    public Map<String, String> getUserRoleOptions() {
+        return dropdownItemProperties.getUserRoleOptions();
+    }
+
+    @ModelAttribute("statusOptions")
+    public Map<String, String> getStatusOptions() {
+        return radioButtonItemProperties.getStatusOptions();
+    }
+}
+
